7dc82d2a52639a1b07e08d537219b7df9ee04e1c
selva-rajT <selvaraj@centizen.com>
2023-06-13 15:56:19 +0530

    otp page work pannen

--- src/components/otpPage/otpPage.js
+++ src/components/otpPage/otpPage.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import {validate} from 'react-email-validator';
 import './otpPage.css';
 import '../loginPage/loginPage.css';
 import Navbar from '../navBar/navBar';
 import harry1 from '../../assets/harry1.png';
-import { getOtp } from '../../services/otpService';
+import { getOtp,verifyOtp } from '../../services/otpService';
+
 
 const OtpPage = () => {
   const [loading, setLoading] = useState(false);
   const [showOtp,setShowOtp]=useState(false);
-  const [disable,setDisable]=useState(false);//disable the send otp button
-  const [error,setError]=useState(false) ; ////see  1
+  const [error,setError]=useState('') ; ////see  1
+  const [alert,setAlert]=useState(false); //showing alert on email validation
+  const [reSend,setResend]=useState(false); //to show the resend link
   const [email, setEmail] = useState('');
   const [otp, setOtp] = useState('');
   const [sec, setSec] = useState(10);
-
+  const[isRunning,SetRunning]=useState(false);//set the clock
+  
   useEffect(() => {
     let timer;
-
-    if (email&&showOtp) {
+    if (email&&isRunning) {
       timer = setInterval(() => {
         setSec(prevSec => {
           if (prevSec === 0) {
@@ -32,36 +35,83 @@ const OtpPage = () => {
     return () => {
       clearInterval(timer);
     };
-  }, [email,showOtp]);
+  }, [email,isRunning]);
+
+  useEffect(()=>{
+    if(sec==0)
+          setResend(true);
+  },[sec]);
 
   const handleEmailChange = event => {
     setEmail(event.target.value);
   };
-
   const handleOtpChange = event => {
     setOtp(event.target.value);
   };
+
   const sendCode=(event)=>{
     event.preventDefault();
-    setLoading(true);
-    getOtp(email)
-      .then(res=>{
-        setLoading(false);
-        setShowOtp(true);
-      })
-      .catch(err=>{
-        console.log(err);
-        setLoading(false);
-        setShowOtp(false);
-      })
+    if(validate(email)){
+        setLoading(true);
+        getOtp(email)
+          .then(res=>{
+              setAlert(false);
+              setError('')
+              setLoading(false);
+              setShowOtp(true);
+              SetRunning(true);
+            })
+          .catch(err=>{
+          console.log(err);
+          setLoading(false);
+          setShowOtp(false);
+            })
+    }
+    else {
+      setAlert(true);
+      setError('Enter correct email id..');
+    }
+    
      
   }
   const handleSubmit=(event)=>{
     event.preventDefault();
-    console.log(email);
-   
+    if(otp){
+      setLoading(true);
+      verifyOtp(otp)
+        .then(res=>{
+          ///password change page
+          setLoading(false);
+        })
+        .catch(err=>{
+          setAlert(true);
+          setError('wrong otp')
+          setLoading(false);
+        });
+    }
   }
 
+  // =========resend the otp
+ const resendCode=(event)=>{
+  event.preventDefault();
+  if(validate(email)){
+    setLoading(true);
+    getOtp(email)
+      .then(res=>{
+          setLoading(false);
+          setSec(10);
+          SetRunning(true);
+          setResend(false);
+      })
+      .catch(err=>{
+        // setLoading(false);
+        // setAlert(true);
+        // setError('error in resending code');
+        console.log(err);
+
+      })
+  }
+ }
   return (
     <div className='container'>
       <Navbar />
@@ -70,7 +120,9 @@ const OtpPage = () => {
         <div className='form-container'>
           <form>
             <h2 className='h2'>OTP Verification</h2>
+            {(alert)?<p className='error'>{error}</p>:null}
             {loading ? <div className='loader1'></div> : null}
+            {!showOtp?<span>
             <label htmlFor='email'>Email :</label>
             <div className='email_container'>
               <input
@@ -79,9 +131,10 @@ const OtpPage = () => {
                 required
                 onChange={handleEmailChange}
               />
-              <button onClick={sendCode} disabled={disable}>Send code</button>
+              <button onClick={sendCode} >Send code</button>
             </div>
-            {(!showOtp)?<p>enter email to get the verification code.</p>:null}
+            </span>:null}
+           
             {showOtp?<span>
             <label htmlFor='otp'>Verification Code :</label>
             <input
@@ -94,11 +147,12 @@ const OtpPage = () => {
             <br />
             <div className='button-container'>
               <button onClick={handleSubmit}>Submit</button>
-              <button onClick={()=>setOtp('')}>Clear</button>
+              <button onClick={()=>{setOtp('');setError('')}}>Clear</button>
             </div>
             <p className='register'>OTP is valid for {sec} seconds</p>
             </span>:null}
-            {error?<p>error....please retry..</p>:null}             {/* ---------- 1--------  */}
+            {reSend?<button className='resend' disabled={!reSend} onClick={resendCode}>Resend the code</button>:null}            {/* ---------- 1--------  */}
+            
           </form>
         </div>
         <div className='quote-container'>
--- src/services/otpService.js
+++ src/services/otpService.js
@@ -2,10 +2,17 @@ import axios from "axios";
 
 axios.defaults.baseURL='http://172.16.0.51:5000/user';
 const url='/v1';
+const verifyotp='/v2';
 
 export const getOtp=async function(userEmail){
-    console.log(userEmail);
+
     return axios.get(url,{
         params:{email:userEmail}
     });
+}
+
+export const verifyOtp=async function(userOtp){
+    return axios.get(verifyotp,{
+        params:{otp:userOtp}
+    })
 }
